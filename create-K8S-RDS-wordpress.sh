37635228f4735a9c6ef04a10dd7343936ce77355
erki-karblane <58291649+erki-karblane@users.noreply.github.com>
2020-06-02 09:24:37 +0300

    Add files via upload

---
+++ create-K8S-RDS-wordpress.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# create EKS cluster with worker nodes, new VPC, subnets, sg, etc
+echo "Creating EKS cluster"
+eksctl create cluster --name EKS-CLUSTER3 --version 1.16 --region eu-west-2 --nodegroup-name standard-workers --node-type t2.micro --nodes 3 --nodes-min 1 --nodes-max 4 --ssh-access --ssh-public-key public_key.pem --managed
+ 
+sleep 300
+echo "EKS cluster with corresponding VCP, SG,nodes and Nat GW complete"
+# create RDS instance with networking
+echo "Creating RDS database with networks, network group and ACL to be accessed by EKS"
+echo " If You want to change the wordpress sql username and password, edit the vars.yml before proceeding"
+ansible-playbook -v RDS.yml
+echo "Waiting for the RDS instance to come up"
+sleep 1200
+# deploy the wordpress kubernetes deployment
+echo "RDS is up, configuring wordpress deployment in kubernetes"
+ansible-playbook -v kube.yml
+sleep 180
+echo "Wordpress deployment is operation"
+# when the wordpress pod is operational, look up the loadbalancers URL for the service
+echo "Please use the below URL to access the wordpress site"
+kubectl get svc | grep wordpress
+echo "Thats it, the Wordpress site is up, please configure it"
---
+++ prerequisites.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+ 
+# install awscli
+echo " Installing AWS CLI"
+apt-get update
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+apt install -y unzip
+unzip awscliv2.zip
+sudo ./aws/install
+# configure aws cli
+echo "Configureing AWS"
+echo "Use eu-west-2 as the region"
+aws configure
+# install kubectl
+echo "Installing kubectl"
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.8/2020-04-16/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+#optional
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc 
+# install eksctl
+echo "Installing eksctl"
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+mv /tmp/eksctl /usr/local/bin
+# installing pyhton3
+echo "Installing Python3"
+apt-get install software-properties-common
+add-apt-repository ppa:deadsnakes/ppa
+apt-get update
+apt-et install pyhton3.6
+# installing pip
+echo "Installing Pyhton pip"
+apt-get install -y python3-pip
+# installing ansible
+echo "Installing ansible"
+pip3 install -U ansible
+# installing openshift module
+echo "Installing opensift module"
+pip3 install -U openshift
+# installing boto, boto3 and botocore
+Echo "Installing botocore and boto with boto3"
+pip3 install -U botocore
+pip3 install -U boto
+pip3 install -U boto3
+
+Echo "Thats it, now Your ready to copy your aws key over."
+Echo " Please use the following to give right permissions and formation: chmod 600 key.pem and ssh-keygen -y -f key.pem > public_key.pem"
+Echo " Testi if Your AWS credentials work for example with a test querry "aws ec2 describe-instances""
